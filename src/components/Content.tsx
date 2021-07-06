45e17855fb8af83d4b52ac43fb36fb9813d2ef1c
rodrigo <digorinaldi@gmail.com>
2021-07-06 12:08:19 -0300

    entrega 00

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
-  // Complete aqui
+import '../styles/content.scss';
+import { MovieCard } from './MovieCard';
+
+interface GenreSelectedProps {
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenre: GenreSelectedProps,
+  movies: Array<MovieProps>
+}
+
+export function Content({ selectedGenre, movies }: ContentProps ) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
